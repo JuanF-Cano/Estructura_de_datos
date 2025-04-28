a66ce2a31f22f8cbff81be2fa37022b470b3304c
JuanF-Cano <juan.cano5@utp.edu.co>
2025-04-27 23:54:41 -0500

    RBT

--- trees/red_black_tree.cc
+++ trees/red_black_tree.cc
@@ -40,6 +40,28 @@ private:
   Node* root_;
   unsigned int size_;
 
+  void preorder(Node* node) const {
+    if (node) {
+      cout << node->getElement() << (node->getColor() == RED ? "R " : "B ");
+      preorder(node->getLeft());
+      preorder(node->getRight());
+    }
+  }
+  void inorder(Node* node) const {
+    if (node) {
+      inorder(node->getLeft());
+      cout << node->getElement() << (node->getColor() == RED ? "R " : "B ");
+      inorder(node->getRight());
+    }
+  }
+  void postorder(Node* node) const {
+    if (node) {
+      postorder(node->getLeft());
+      postorder(node->getRight());
+      cout << node->getElement() << (node->getColor() == RED ? "R " : "B ");
+    }
+  }
+
   void rotateLeft(Node*& root, Node*& x) {
     Node* y = x->getRight();
     x->setRight(y->getLeft());
@@ -117,27 +139,6 @@ private:
     root->setColor(BLACK);
   }
 
-  void preorder(Node* node) const {
-    if (node) {
-      cout << node->getElement() << (node->getColor() == RED ? "R " : "B ");
-      preorder(node->getLeft());
-      preorder(node->getRight());
-    }
-  }
-  void inorder(Node* node) const {
-    if (node) {
-      inorder(node->getLeft());
-      cout << node->getElement() << (node->getColor() == RED ? "R " : "B ");
-      inorder(node->getRight());
-    }
-  }
-  void postorder(Node* node) const {
-    if (node) {
-      postorder(node->getLeft());
-      postorder(node->getRight());
-      cout << node->getElement() << (node->getColor() == RED ? "R " : "B ");
-    }
-  }
   Node* insert(Node* root, Node* node) {
     if (!root) return node;
     if (node->getElement() < root->getElement()) {
@@ -149,28 +150,164 @@ private:
     return root;
   }
 
-  public:
-    RBT() {
-      root_ = nullptr;
-      size_ = 0;
+  void fixRemove(Node*& root, Node*& node) {
+    while (node != root && node->getColor() == BLACK) {
+      if (node == node->getParent()->getLeft()) {
+        Node* sibling = node->getParent()->getRight();
+        if (sibling->getColor() == RED) {
+          sibling->setColor(BLACK);
+          node->getParent()->setColor(RED);
+          Node* parent = node->getParent();
+          rotateLeft(root, parent);
+          sibling = node->getParent()->getRight();
+        }
+        if (sibling->getLeft()->getColor() == BLACK && sibling->getRight()->getColor() == BLACK) {
+          sibling->setColor(RED);
+          node = node->getParent();
+        } else {
+          if (sibling->getRight()->getColor() == BLACK) {
+            sibling->getLeft()->setColor(BLACK);
+            sibling->setColor(RED);
+            rotateRight(root, sibling);
+            sibling = node->getParent()->getRight();
+          }
+          sibling->setColor(node->getParent()->getColor());
+          node->getParent()->setColor(BLACK);
+          sibling->getRight()->setColor(BLACK);
+          Node* parent = node->getParent();
+          rotateLeft(root, parent);
+          node = root;
+        }
+      } else {
+        Node* sibling = node->getParent()->getLeft();
+        if (sibling->getColor() == RED) {
+          sibling->setColor(BLACK);
+          node->getParent()->setColor(RED);
+          Node* parent = node->getParent(); 
+          rotateRight(root, parent);
+          sibling = node->getParent()->getLeft();
+        }
+        if (sibling->getRight()->getColor() == BLACK && sibling->getLeft()->getColor() == BLACK) {
+          sibling->setColor(RED);
+          node = node->getParent();
+        } else {
+          if (sibling->getLeft()->getColor() == BLACK) {
+            sibling->getRight()->setColor(BLACK);
+            sibling->setColor(RED);
+            rotateLeft(root, sibling);
+            sibling = node->getParent()->getLeft();
+          }
+          sibling->setColor(node->getParent()->getColor());
+          node->getParent()->setColor(BLACK);
+          sibling->getLeft()->setColor(BLACK);
+          Node* parent = node->getParent(); 
+          rotateRight(root, parent);
+          node = root;
+        }
+      }
     }
-    RBT(const T& element) {
-      root_ = new Node(element); 
-      root_->setColor(BLACK);
-      size_ = 1;
+    node->setColor(BLACK);
+  }
+
+  Node* remove(Node* root, const T& val) {
+    if (!root) return root;
+
+    if (val < root->getElement()) {
+      root->setLeft(remove(root->getLeft(), val));
+    } else if (val > root->getElement()) {
+      root->setRight(remove(root->getRight(), val));
+    } else {
+      if (!root->getLeft() || !root->getRight()) {
+        Node* temp = root->getLeft() ? root->getLeft() : root->getRight();
+        if (!temp) {
+          temp = root;
+          root = nullptr;
+        } else {
+          *root = *temp;
+        }
+        delete temp;
+      } else {
+        Node* temp = minimum(root->getRight());
+        root->setElement(temp->getElement());
+        root->setRight(remove(root->getRight(), temp->getElement()));
+      }
     }
-    unsigned int size() const { return size_; }
-    bool empty() const { return (size_ == 0); } 
-    void inorderTraversal() const { inorder(root_); cout << endl; }
-    void preorderTraversal() const { preorder(root_); cout << endl; }
-    void postorderTraversal() const { postorder(root_); cout << endl; }
-    
-    void insert(const T& val) {
-      Node* node = new Node(val);
-      root_ = insert(root_, node);
-      fixInsert(root_, node);
-      size_++;
+
+    if (root != nullptr) fixRemove(root_, root);
+
+    return root;
+  }
+
+  Node* find(Node* node, const T& val) const {
+    if (!node || node->getElement() == val) return node;
+    if (val < node->getElement()) return find(node->getLeft(), val);
+    return find(node->getRight(), val);
+  }
+  Node* minimum(Node* node) const {
+    while (node && node->getLeft()) {
+      node = node->getLeft();
     }
+    return node;
+  }
+  Node* maximum(Node* node) const {
+    while (node && node->getRight()) {
+      node = node->getRight();
+    }
+    return node;
+  }
+  void clear(Node* node) {
+    if (node) {
+      clear(node->getLeft());
+      clear(node->getRight());
+      delete node;
+    }
+  }
+  unsigned int height(Node* node) const {
+    if (!node) return 0;
+    int leftHeight = height(node->getLeft());
+    int rightHeight = height(node->getRight());
+    return max(leftHeight, rightHeight) + 1;
+  }
+
+public:
+  RBT() {
+    root_ = nullptr;
+    size_ = 0;
+  }
+  RBT(const T& element) {
+    root_ = new Node(element); 
+    root_->setColor(BLACK);
+    size_ = 1;
+  }
+  unsigned int size() const { return size_; }
+  bool empty() const { return (size_ == 0); } 
+  void inorderTraversal() const { inorder(root_); cout << endl; }
+  void preorderTraversal() const { preorder(root_); cout << endl; }
+  void postorderTraversal() const { postorder(root_); cout << endl; }
+
+  Node* find(const T& val) const { return find(root_, val); }
+  Node* minimum() const { return minimum(root_); }
+  Node* maximum() const { return maximum(root_); }
+  unsigned int height() const { return height(root_); }
+  
+  void insert(const T& val) {
+    Node* node = new Node(val);
+    root_ = insert(root_, node);
+    fixInsert(root_, node);
+    size_++;
+  }
+
+  void remove(const T& val) {
+    root_ = remove(root_, val);
+    size_--;
+  }
+
+  void clear() {
+    clear(root_);
+    root_ = nullptr;
+    size_ = 0;
+  }
+
 };
 
 int main() {
@@ -188,21 +325,40 @@ int main() {
   cout << "Preorder traversal (root, left, right):" << endl;
   tree.preorderTraversal();
   // Expected: 20B 10R 5B 1R 15B 30B 25R 
-
-  cout << "Inorder traversal (left, root, right):" << endl;
-  tree.inorderTraversal();
-  // Expected: 1R 5B 10B 15R 20B 25B 30R
-
+  
   cout << "Postorder traversal (left, right, root):" << endl;
   tree.postorderTraversal();
   // Expected: 1R 5B 15B 10R 25R 30B 20B
-
+  
   // Check size and if tree is empty
   cout << "Tree size: " << tree.size() << endl;
   // Expected: 7
-
+  
   cout << "Is tree empty?: " << (tree.empty() ? "Yes" : "No") << endl;
   // Expected: No
 
+  // Finding an element
+  if (tree.find(15)) cout << "Found 15 in the tree!" << endl;
+  else cout << "15 not found." << endl;
+  
+  // Minimum and maximum
+  cout << "Minimum: " << tree.minimum()->getElement() << endl;
+  cout << "Maximum: " << tree.maximum()->getElement() << endl;
+  
+  // Tree height
+  cout << "Tree height: " << tree.height() << endl;  
+
+  cout << "Inorder traversal (left, root, right):" << endl;
+  tree.inorderTraversal();
+  // Expected: 1R 5B 10R 15B 20B 25R 30B
+  
+  tree.remove(10);
+  cout << "After removing 10: ";
+  tree.inorderTraversal();
+
+  // Clear the tree
+  tree.clear();
+  cout << "Tree cleared, size: " << tree.size() << endl;
+  
   return 0;
 }
